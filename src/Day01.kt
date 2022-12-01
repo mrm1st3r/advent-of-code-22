e8449ad0bd390ff1593d40b21d6dc1b778b28f79
Lukas Taake <lukas.taake@holisticon.de>
2022-12-01 22:32:03 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
+    fun elfCalories(input: List<String>): MutableList<Int> {
+        val calories = MutableList(input.size) { 0 }
+
+        var currentElf = 0
+        for (item in input) {
+            if (item.isEmpty()) {
+                currentElf++
+            } else {
+                calories[currentElf] = calories[currentElf] + item.toInt()
+            }
+        }
+        return calories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = elfCalories(input)
+
+        return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return elfCalories(input)
+                .sorted()
+                .reversed()
+                .subList(0, 3)
+                .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Test file")
+    val part1 = part1(testInput)
+    println(part1)
+    check(part1 == 24000)
+    val part2 = part2(testInput)
+    println(part2)
+    check(part2 == 45000)
 
     val input = readInput("Day01")
+    println("Real file")
     println(part1(input))
     println(part2(input))
 }

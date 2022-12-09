08f45261f0848d2ca142ada131ea80c27b24609b
Lukas Taake <lukas.taake@holisticon.de>
2022-12-09 16:20:51 +0100

    add template package

---
+++ src/newday/newday.kt
@@ -0,0 +1,22 @@
+package newday
+
+import Puzzle
+
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    Puzzle(
+        "day4",
+        ::part1,
+        0,
+        ::part2,
+        0
+    ).test()
+}
